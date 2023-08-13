87946aaf5af27349f723f40b3954a4a5413eb13e
Bhavin Chavda <bhavinchavada242846@gmail.com>
2023-08-13 23:49:13 +0530

    i have make base_app and create teplates folder inside base_app, i create static forlder inside E-shopper project folder, i make urls file in base_app to connect with project urls file for rendering, i create base.html template inside template folder inside base_app, i make changes in base.html like add load static and add {% static 'url_path' %} for connect to static forlder css ,js,and other file

---
+++ E_shopper/base_app/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from base_app import views
+
+urlpatterns = [
+
+    path('', views.baseview),
+    
+]
--- E_shopper/base_app/views.py
+++ E_shopper/base_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def baseview(request):
+    return render(request,'base_app/base.html')
