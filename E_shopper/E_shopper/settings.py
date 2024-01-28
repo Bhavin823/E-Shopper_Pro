f86fcee5771df44e836c10de2d3f146f0f93cfb0
Bhavin Chavada <80876539+Bhavin823@users.noreply.github.com>
2024-01-28 17:38:11 +0530

    Update allowed hosts

--- E_shopper/E_shopper/settings.py
+++ E_shopper/E_shopper/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = 'django-insecure-hxv9tg85@0w825g#!(#_--g==i(vvi-dsy560qns$i^g#_$bm0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['e-shopper-pl0g.onrender.com']
+# ALLOWED_HOSTS = ['e-shopper-pl0g.onrender.com']
+
+# settings.py
+
+ALLOWED_HOSTS = ['e-shopper-pl0g.onrender.com', 'subdomain.e-shopper-pl0g.onrender.com', '192.168.1.100']
+
 
 
 # Application definition
