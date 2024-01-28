d751e4ba6fc12908308e885e69c246bd89db00d9
Bhavin Chavada <80876539+Bhavin823@users.noreply.github.com>
2024-01-28 16:17:15 +0530

    add 'e-shopper-pl0g.onrender.com' in alowed hosts in setting.py

--- E_shopper/E_shopper/settings.py
+++ E_shopper/E_shopper/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hxv9tg85@0w825g#!(#_--g==i(vvi-dsy560qns$i^g#_$bm0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['e-shopper-pl0g.onrender.com']
 
 
 # Application definition
