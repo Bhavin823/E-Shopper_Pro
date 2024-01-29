63d3cd453fca94198fb20a62ee13060b12f16fbf
Bhavin Chavada <80876539+Bhavin823@users.noreply.github.com>
2024-01-29 15:30:19 +0530

    make some changes in staticfile dirs

--- E_shopper/E_shopper/settings.py
+++ E_shopper/E_shopper/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'E-Shopper_Pro/E_shopper/static')
+    os.path.join(BASE_DIR, 'E-Shopper_Pro/E_shopper/staticfiles')
     ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
