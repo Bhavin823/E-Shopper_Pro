416a53682a4cceddf231b8a8d8adb1c6e2dfd616
Bhavin Chavada <80876539+Bhavin823@users.noreply.github.com>
2024-01-30 11:05:58 +0530

    make changes in static

--- E_shopper/E_shopper/settings.py
+++ E_shopper/E_shopper/settings.py
@@ -163,10 +163,16 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # as per render doc
-
 STATIC_URL = '/static/'
+
+# Following settings only make sense on production and may break development environments.
 if not DEBUG:
-    STATIC_ROOT = '/opt/render/project/src/E_shopper/staticfiles'
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # as per redner doc
 
